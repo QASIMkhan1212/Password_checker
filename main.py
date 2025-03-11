4fbdb8bf188a4a381d220f4f2e7c4b64ab02db40
M. QASIM <qasimygv@gmail.com>
2025-03-11 06:43:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+    
+    # Length Check
+    if len(password) >= 8:
+        score += 1
+    else:
+        print("❌ Password should be at least 8 characters long.")
+    
+    # Upper & Lowercase Check
+    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+        score += 1
+    else:
+        print("❌ Include both uppercase and lowercase letters.")
+    
+    # Digit Check
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        print("❌ Add at least one number (0-9).")
+    
+    # Special Character Check
+    if re.search(r"[!@#$%^&*]", password):
+        score += 1
+    else:
+        print("❌ Include at least one special character (!@#$%^&*).")
+        
+    # rejecting weak paswords
+    if password in ["password", "123456", "qwerty", "admin", "user","pakistan","abcde","wxyv","123456789","12345678","1234567","1234567890","12345"]:
+        print("❌ Weak Password - Please choose a stronger password.")
+        return
+      # adding a feature to suggest a strong pasword
+    if score < 4:
+        print("💡 Suggested Password: 4aB@1234")
+
+      
+    # Strength Rating
+    if score == 4:
+        print("✅ Strong Password!")
+    elif score == 3:
+        print("⚠️ Moderate Password - Consider adding more security features.")
+    else:
+        print("❌ Weak Password - Improve it using the suggestions above.")
+
+# Get user input
+password = input("Enter your password: ")
+check_password_strength(password)
